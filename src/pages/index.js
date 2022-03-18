60c8569969a8e531508df9f140e39426e88017b4
Curtis Babin <curtisbabin@Curtiss-MacBook-Pro-10-Core-14-Inch.local>
2022-03-17 19:02:32 -0700

    Site construction page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,27 @@
-import * as React from "react"
-
+import * as React from "react";
+import "../styles/global.css";
+import { StaticImage } from "gatsby-plugin-image"
 
 const IndexPage = () => {
   return (
-    <main>
+    <>
       <title>Stevie's Details | Home</title>
 
-      <h1>Stevie's Details</h1>
-      <h2>Located in Orange County, California</h2>
+      <main>
+        <div className="center">
+          <h1>Stevie's Details</h1>
+          <StaticImage src="../images/smallLogo.png" alt="Stevie's Details logo" width={500} height={400} />
+          <h2>Located in Orange County, California</h2>
 
-      <h3>This site is under construction until ~ April, 2022</h3>
-    </main>
+          <h3>This site is under construction until ~ April, 2022</h3>
+
+          <a href="https://www.instagram.com/stevies_details/" target="_blank" rel="noreferrer">Instagram</a>
+          <a href="https://www.tiktok.com/@stevies_details" target="_blank" rel="noreferrer">TikTok</a>
+          <a href="mailto: steviesdetailsllc@gmail.com" target="_blank" rel="noreferrer">Email</a>
+        </div>
+
+      </main>
+    </>
   )
 }
 
