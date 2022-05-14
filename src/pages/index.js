d357887abab15ebb0d687946056514ed8166fb34
Curtis Babin <curtsocal@gmail.com>
2022-05-14 14:14:50 -0700

    Update construction date & set link color to white

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,10 +19,12 @@ const IndexPage = () => {
           <StaticImage src="../images/smallLogo.png" alt="Stevie's Details logo" width={500} height={400} />
           <h2>Located in Orange County, California</h2>
 
-          <h3>This site is under construction until ~ April, 2022</h3>
+          <h3>This site is under construction until ~ June, 2022</h3>
+
 
           <a href="https://www.instagram.com/stevies_details/" target="_blank" rel="noreferrer">Instagram</a>
           <a href="https://www.tiktok.com/@stevies_details" target="_blank" rel="noreferrer">TikTok</a>
+          <a href="https://www.yelp.com/biz/stevie-s-details-santa-ana-2" target="_blank" rel="noreferrer">Yelp</a>
           <a href="mailto: steviesdetailsllc@gmail.com" target="_blank" rel="noreferrer">Email</a>
         </div>
 
