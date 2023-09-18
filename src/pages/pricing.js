4b3fa84f747bc6960ceef4c8baa89ea9dc653a0a
Curtis Babin <curtsocal@gmail.com>
2023-09-17 17:04:12 -0700

    IDK how things got messed up, but this is the fix

--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -8,13 +8,6 @@ const PricingPage = () => {
   const contentDescription = "Our Prices";
   const canonicalLink = "https://steviesdetails.com/pricing";
 
-  /*
-  https://www.instagram.com/stevies_details/
-  https://www.yelp.com/biz/stevie-s-details-santa-ana-2
-  https://www.tiktok.com/@stevies_details
-  steviesdetailsllc [at] gmail [dot] com
-  */
-
   return (
     <PageLayout 
       pageTitle={pageTitle} 
